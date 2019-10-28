9d8cb847021a8082dbb8270421887ce49aa1c7eb
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-10-28 12:17:20 -0700

    Started up project

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db.js");
+const server = express();
+server.use(express.json());
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
+    });
+});
+server.listen(8000, () => console.log("server is running :)"));
